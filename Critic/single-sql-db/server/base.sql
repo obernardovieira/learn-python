256d41c5eb52781398dcac9c271a54e973ad4620
Bernardo Vieira <bernardo5@live.com.pt>
2017-11-14 23:51:40 +0000

    sql script updated

--- Critic/single-sql-db/server/base.sql
+++ Critic/single-sql-db/server/base.sql
@@ -1,1 +1,3 @@
+CREATE DATABASE singlesqldb;
+USE singlesqldb;
 CREATE TABLE client(firstname VARCHAR(32), lastname VARCHAR(32), address VARCHAR(32), status VARCHAR(32), animal VARCHAR(32), car VARCHAR(32));
